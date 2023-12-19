7ed454f86b8600bdf195ab1330306bbaf7f46c91
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-12-19 11:58:06 -0300

    Responsividade para os botões do componente Home.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
                     <div className={`${[styles.firstColumn]}`}>
                         <h1 className={styles.h1}>Desenvolvedor FullStack</h1>
                         <span><TypingAnimation className={styles.typingAnimation} text="Profissional que abrange as duas áreas, todavia me considero com destaques em Front-End." /></span>
-                        <div className="d-flex d-lg-flex d-none">
+                        <div className={`${[styles.btn]} d-flex`}>
                             <a href="https://www.linkedin.com/in/devrafaelcunha/" target="blank" type="button" className="custom-btn btn-1 mt-5">Linkedin</a>
                             <a href="https://github.com/dev-rafaelcunha" target="blank" type="button" className="custom-btn btn-2 mt-5 mx-5">Github</a>
                             <a href="https://www.instagram.com/dev.rafaelcunha/?next=%2F" target="blank" type="button" className="custom-btn btn-3 mt-5">Instagram</a>
