62dbf7ad2832b37467b4b33975498430bb6b55e6
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-12-19 12:43:29 -0300

    Ajuste posição dos botões no componente Home.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
                         <span><TypingAnimation className={styles.typingAnimation} text="Profissional que abrange as duas áreas, todavia me considero com destaques em Front-End." /></span>
                         <div className={`${[styles.btn]} d-flex`}>
                             <a href="https://www.linkedin.com/in/devrafaelcunha/" target="blank" type="button" className="custom-btn btn-1 mt-5">Linkedin</a>
-                            <a href="https://github.com/dev-rafaelcunha" target="blank" type="button" className="custom-btn btn-2 mt-5 mx-5">Github</a>
+                            <a href="https://github.com/dev-rafaelcunha" target="blank" type="button" className="custom-btn btn-2 mt-5">Github</a>
                             <a href="https://www.instagram.com/dev.rafaelcunha/?next=%2F" target="blank" type="button" className="custom-btn btn-3 mt-5">Instagram</a>
                         </div>
                     </div>
