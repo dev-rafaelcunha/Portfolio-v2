34460683c5117bf306a8e589e15e9c4ecdce7bc5
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2024-01-21 17:54:34 -0300

    Feito a migração do Home CSS para Styled Components.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,86 @@
 import Header from "../layout/Header";
 import TypingAnimation from "../utils/TypingAnimation";
-import styles from "./Home.module.css";
+import styled from "styled-components";
+
+const Main = styled.main`
+
+    height: 90vh;
+    background-color: #111;
+
+    @media (max-width: 991px) {
+
+        padding: 0 !important;
+    }
+`
+
+const Grid = styled.main`
+
+    height: 80vh;
+    width: 100vw;
+
+    @media (max-width: 991px) {
+
+        display: flex !important;
+        padding: 0 30px;
+    }
+`
+const FirstColumn = styled.main`
+
+    background-color: #111;
+    z-index: 100;
+    width: 40% !important;
+
+    @media (max-width: 991px) {
+
+        text-align: center !important;
+        width: 100% !important;
+    }
+`
+
+const Title = styled.main`
+
+    font-size: 3rem;
+    color: #fff;
+
+    @media (max-width: 991px) {
+
+        font-size: 2.5rem;
+        white-space: normal !important;
+    }
+`
+
+const SecondColumn = styled.main`
+
+    right: 0;
+`
+
+const Logo = styled.main`
+
+    font-size: 330px;
+    font-family: monospace;
+    font-weight: bold;
+    color: transparent;
+    background-image: linear-gradient(to left, transparent, #f52754);
+    -webkit-background-clip: text;
+    background-clip: text;
+
+    @media (max-width: 1200px) {
+        
+        display: none !important;
+    }
+`
+
+const SocialButton = styled.main`
+
+    width: 590px;
+    display: flex;
+
+    @media (max-width: 991px) {
+
+        justify-content: center !important;
+        width: auto !important;
+    }
+`
 
 function Home() {
 
@@ -10,24 +90,24 @@ function Home() {
 
         <div className="container-fluid p-0">
             <Header />
-            <main className={`${[styles.main]} px-5`}>
+            <Main className='px-5'>
                 <div className="grid-layout">
-                    <div className={`${[styles.grid]} d-flex align-items-center`}>
-                        <div className={`${[styles.firstColumn]}`}>
-                            <h1 className={`${[styles.h1]} text-nowrap mb-4`}>Desenvolvedor FullStack</h1>
-                            <span><TypingAnimation className={styles.typingAnimation} text="Profissional que abrange as duas áreas, todavia me considero com destaques em Front-End." /></span>
-                            <div className={`${[styles.btn]} d-flex`}>
+                    <Grid className='d-flex align-items-center'>
+                        <FirstColumn>
+                            <Title className='text-nowrap mb-4'>Desenvolvedor FullStack</Title>
+                            <TypingAnimation className='typingAnimation' text="Profissional que abrange as duas áreas, todavia me considero com destaques em Front-End." />
+                            <SocialButton>
                                 <a href="https://www.linkedin.com/in/devrafaelcunha/" target="blank" type="button" className="custom-btn btn-1 mt-5">Linkedin</a>
                                 <a href="https://github.com/dev-rafaelcunha" target="blank" type="button" className="custom-btn btn-2 mt-5">Github</a>
                                 <a href="https://www.instagram.com/dev.rafaelcunha/?next=%2F" target="blank" type="button" className="custom-btn btn-3 mt-5">Instagram</a>
-                            </div>
-                        </div>
-                        <div className={`${[styles.secondColumn]} position-absolute`}>
-                            <span className={`${[styles.logo]}`}>{logo}</span>
-                        </div>
-                    </div>
+                            </SocialButton>
+                        </FirstColumn>
+                        <SecondColumn className='position-absolute'>
+                            <Logo>{logo}</Logo>
+                        </SecondColumn>
+                    </Grid>
                 </div>
-            </main>
+            </Main>
         </div>
     )
 }
